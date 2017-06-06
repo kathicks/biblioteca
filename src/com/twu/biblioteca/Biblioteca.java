ca62b4d111e07078f02b3953fa9f1b80f7233354
kathicks <kmhicks92@gmail.com>
2017-06-06 11:25:17 +0100

    Prints list of books after welcome message

--- src/com/twu/biblioteca/Biblioteca.java
+++ src/com/twu/biblioteca/Biblioteca.java
@@ -4,12 +4,34 @@ public class Biblioteca {
 
     public static void main(String[] args) {
         Biblioteca self = new Biblioteca();
-        self.welcomeMessage();
+
+        System.out.println(self.welcomeMessage() + "\n");
+        System.out.println(self.listBooks());
+    }
+
+    private String[] books = new String[] {
+            "Half of a Yellow Sun",
+            "Things Fall Apart",
+            "Norwegian Wood",
+            "The Buried Giant",
+            "Brave New World"
+    };
+
+    public String[] getBooks() {
+        return books;
     }
 
     public String welcomeMessage() {
-        String message = "Welcome to Biblioteca!";
-        System.out.println(message);
-        return message;
+        return "Welcome to Biblioteca!";
     }
+
+    public String listBooks() {
+        String result = "";
+        for (String book : books) {
+            result += book;
+            result += '\n';
+        }
+        return result;
+    }
+
 }
--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -1,14 +1,43 @@
 package com.twu.biblioteca;
 
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import org.junit.*;
+import static org.junit.Assert.*;
 
 public class BibliotecaTest {
 
+    Biblioteca biblioteca;
+    String[] books;
+    String list;
+
+    @Before
+    public void setUp() {
+        biblioteca = new Biblioteca();
+        books = new String[] {
+                    "Half of a Yellow Sun",
+                    "Things Fall Apart",
+                    "Norwegian Wood",
+                    "The Buried Giant",
+                    "Brave New World"
+                };
+        list = "Half of a Yellow Sun\n" +
+                "Things Fall Apart\n" +
+                "Norwegian Wood\n" +
+                "The Buried Giant\n" +
+                "Brave New World\n";
+    }
+
     @Test
     public void testWelcomeMessage() {
-        Biblioteca biblioteca = new Biblioteca();
         assertEquals(biblioteca.welcomeMessage(), "Welcome to Biblioteca!");
     }
 
+    @Test
+    public void testGetBooks() {
+        assertArrayEquals(biblioteca.getBooks(), books);
+    }
+
+    @Test
+    public void testListBooks()  {
+        assertEquals(biblioteca.listBooks(), list);
+    }
 }
